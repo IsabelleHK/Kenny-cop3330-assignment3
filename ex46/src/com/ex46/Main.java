bf287d17c46d909c001884a9d30d77ab59ac65f3
Isabelle Kenny <90565341+IsabelleHK@users.noreply.github.com>
2021-10-10 17:03:20 -0400

    Add files via upload

---
+++ ex46/src/com/ex46/Main.java
@@ -0,0 +1,52 @@
+package com.ex46;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args)throws IOException {
+        String currentPath = System.getProperty("user.dir");
+        File file = new File(currentPath +"\\exercise46_input.txt");
+        String[] words=null; //Intialize the word Array
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String s;
+        String word1="badger", word2 = "mushroom", word3="snake";
+        int count1=0, count2=0, count3=0;
+        while((s=br.readLine())!=null){
+            words=s.split(" ");
+            for (String word : words) {
+                if (word.equals(word1)){
+                    count1++; 
+                }
+                if(word.equals(word2)){
+                    count2++;
+                }
+                if(word.equals(word3)){
+                    count3++;
+                }
+            }
+        }
+        System.out.print("badger: ");
+        if(count1!=0){
+            for(int i = 0; i<count1; i++){
+                System.out.print("*");
+            }
+        }
+        System.out.print("\nmushroom: ");
+        if(count2!=0){
+            for(int i = 0; i<count2; i++){
+                System.out.print("*");
+            }
+        }
+        System.out.print("\nsnake: ");
+        if(count3!=0){
+            for(int i = 0; i<count3; i++){
+                System.out.print("*");
+            }
+        }
+        fr.close();
+    }
+}
