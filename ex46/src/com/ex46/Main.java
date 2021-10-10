54ed3e497721b0e9468aea754af6c8050ae23596
Isabelle Kenny <90565341+IsabelleHK@users.noreply.github.com>
2021-10-10 18:16:40 -0400

    Update Main.java

--- ex46/src/com/ex46/Main.java
+++ ex46/src/com/ex46/Main.java
@@ -1,4 +1,8 @@
 package com.ex46;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Isabelle Kenny
+ */
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
